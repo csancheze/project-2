73116d702f31bcbc561be89c29ad8866f7e400c7
Regulo Argotte <rargotte@hotmail.com>
2022-03-01 20:46:51 -0600

    Sort formula applied to Profile, Category and MyEvents

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,4 +6,14 @@ module.exports = {
   format_time: (date) => {
     return date.toLocaleTimeString();
   },
+
+  compare: ( a, b )=> {
+    if ( a.last_nom < b.last_nom ){
+      return -1;
+    }
+    if ( a.last_nom > b.last_nom ){
+      return 1;
+    }
+    return 0;
+  }
 }
