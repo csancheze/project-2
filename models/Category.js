028a385b3ddd281884fecf77891acd389fe20602
Isaías Reyes León <elreyieon@Isaiass-MacBook-Air.local>
2022-03-03 20:11:31 -0600

    Updated addtags and homepage handlebars. Also added icon to category model.

--- models/Category.js
+++ models/Category.js
@@ -15,6 +15,10 @@ Category.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    icon: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
