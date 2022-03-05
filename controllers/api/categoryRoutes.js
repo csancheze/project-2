eaaa0412b27c4e060f5de8b9b1bcc32dbffbdc6f
Cesar Sanchez <cesanchezesc@gmail.com>
2022-03-04 19:46:22 -0600

    added category update route for setup

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -19,6 +19,26 @@ router.post('/', withAuth, async (req, res) => {
 
 });
 
-
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const categoryData = await Category.update({
+        icon: "bi bi-heart"
+      },
+      {
+        where: {
+          name : "Health"
+        },
+      }
+      );
+      if (!categoryData) {
+        res.status(404).json({message: 'No Category found with this id!'});
+        return;
+      }
+      res.status(200).json(categoryData)
+  
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
